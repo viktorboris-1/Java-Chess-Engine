7c6b7983e510134571418d195ef2e402d72e1321
Viktor Boris <boris035@umn.edu>
2023-10-04 19:52:10 -0400

    Finished stubing out Pawn

--- src/com/chess/engine/pieces/Pawn.java
+++ src/com/chess/engine/pieces/Pawn.java
@@ -4,6 +4,7 @@ import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
+import com.google.common.collect.ImmutableList;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -12,7 +13,7 @@ import java.util.List;
 import static com.chess.engine.board.Move.*;
 
 public class Pawn extends Piece{
-    private final static int[] CANDIDATE_MOVE_COORDINATES = {8, 16};
+    private final static int[] CANDIDATE_MOVE_COORDINATES = {8, 16, 7, 9};
     Pawn(int piecePosition, Alliance pieceAlliance) { super(piecePosition, pieceAlliance); }
 
     @Override
@@ -30,21 +31,42 @@ public class Pawn extends Piece{
             if (currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
                 //TODO Temporary legalMoves addition must flush out
                 legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
-            }
-            else if (currentCandidateOffset == 16 && this.isFirstMove() && (BoardUtils.SECOND_ROW[this.piecePosition] &&
-                    this.getPieceAlliance().isBlack()) || (BoardUtils.SEVENTH_ROW[this.piecePosition] &&
-                    this.getPieceAlliance().isWhite())) {
+            } else if (currentCandidateOffset == 16 && this.isFirstMove() && (BoardUtils.SECOND_ROW[this.piecePosition] &&
+                    this.pieceAlliance.isBlack()) || (BoardUtils.SEVENTH_ROW[this.piecePosition] &&
+                    this.pieceAlliance.isWhite())) {
 
-                final int behindCandidateDestinationCorrdinate = this.piecePosition + (this.pieceAlliance.getDirection() * 8);
+                final int behindCandidateDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * 8);
 
-                if (!board.getTile(behindCandidateDestinationCorrdinate).isTileOccupied() &&
+                if (!board.getTile(behindCandidateDestinationCoordinate).isTileOccupied() &&
                         !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
                     //TODO Temporary legalMoves addition must flush out
                     legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
                 }
 
+            } else if (currentCandidateOffset == 7 &&
+                    !((BoardUtils.EIGHT_COLUMN[this.piecePosition] && this.pieceAlliance.isWhite() ||
+                            (BoardUtils.FIRST_COLUMN[this.piecePosition] && this.pieceAlliance.isBlack())))) {
+
+                if (board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                    final Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+                    if (this.pieceAlliance != pieceOnCandidate.getPieceAlliance()) {
+                        // TODO more to fill in here
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                    }
+                }
+            } else if (currentCandidateOffset == 7 &&
+                    !((BoardUtils.EIGHT_COLUMN[this.piecePosition] && this.pieceAlliance.isBlack() ||
+                            (BoardUtils.FIRST_COLUMN[this.piecePosition] && this.pieceAlliance.isWhite())))) {
+
+                if (board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                    final Piece pieceOnCandidate = board.getTile(candidateDestinationCoordinate).getPiece();
+                    if (this.pieceAlliance != pieceOnCandidate.getPieceAlliance()) {
+                        // TODO more to fill in here
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                    }
+                }
             }
         }
-        return null;
+        return ImmutableList.copyOf(legalMoves);
     }
 }
